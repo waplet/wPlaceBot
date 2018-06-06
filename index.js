cd514d0b8fbdec9a7609faaefd358e00f18e8f49
waplet <waplet@gmail.com>
2018-06-07 00:05:58 +0300

    chat it

--- index.js
+++ index.js
@@ -1,13 +1,17 @@
 const Telegraf = require('telegraf');
 
-const bot = new Telegraf(process.env.BOT_TOKEN);
+const BOT_TOKEN = process.env.BOT_TOKEN;
+const CHAT_ID = process.env.CHAT_ID;
+
+const bot = new Telegraf(BOT_TOKEN);
 /** @var {Array} */
 const places = require('./resources/places');
 
-bot.start((ctx) => {
-    return ctx.reply('Working');
-});
 bot.command('/getplace', (ctx) => {
+    if (CHAT_ID && ctx.chat.id.toString() !== CHAT_ID) {
+        return;
+    }
+
     let countPlaces = places.length;
     let randomPlaceId = Math.floor(Math.random() * countPlaces);
 
